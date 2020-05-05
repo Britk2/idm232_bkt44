939f3bd4555e2f315ed1225b02c9589f444ca3be
Britk2 <55754895+Britk2@users.noreply.github.com>
2020-05-04 21:02:39 -0400

    Filter & Help page + More style

--- java.js
+++ java.js
@@ -1,9 +1,11 @@
 const fill = document.getElementById('fill'); // filter mode
-const filter_b = document.getElementById('filter_b') //filter button
+const filter_buts = document.querySelectorAll('.filter_b') //filter button
+const filter_b = document.getElementById('filter_b');
 const search_b = document.getElementById('search_b'); //search button
 const search_field = document.getElementById('search_bar') // search field
 const info = document.getElementById('info'); // help mode
-const help = document.getElementById('quest') // help button
+const help_buts = document.querySelectorAll('.quest'); // help button
+const help_b = document.getElementById('quest');
 
 console.log(fill);
 
@@ -11,15 +13,34 @@ console.log(fill);
 
 search_b.addEventListener('click', () => {
     search_field.hidden = !search_field.hidden;
+    
+    if (help_b.hidden) {
+        filter_b.hidden = !filter_b.hidden;
+        help_b.hidden = !help_b.hidden;
+        if (!info.hidden) {
+            info.hidden = !info.hidden;
+        }
+        if (!fill.hidden) {
+            fill.hidden = !fill.hidden;
+        }
+    }
     console.log("toggle search");
 })
 
-filter_b.addEventListener('click', () => {
-    fill.hidden = !fill.hidden;
-    console.log("toggle filter");
+filter_buts.forEach(button => {
+    button.addEventListener('click', () => {
+        fill.hidden = !fill.hidden;
+        filter_b.hidden = !filter_b.hidden;
+        help_b.hidden = !help_b.hidden;
+        console.log("toggle filter");
+    })
 })
 
-help.addEventListener('click', () => {
-    info.hidden = !info.hidden;
-    console.log("toggle help");
+help_buts.forEach(button => {
+    button.addEventListener('click', () => {
+        info.hidden = !info.hidden;
+        filter_b.hidden = !filter_b.hidden;
+        help_b.hidden = !help_b.hidden;
+        console.log("toggle help");
+    })
 })
