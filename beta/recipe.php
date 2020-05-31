3c8e9fd59b0c2a811292052d4e129169aecc89ed
Britk2 <55754895+Britk2@users.noreply.github.com>
2020-05-31 16:16:47 -0400

    Header Fixed & Large Screen Alt Steps

--- beta/recipe.php
+++ beta/recipe.php
@@ -92,6 +92,26 @@
 
                 <?php
                 
+                function stepToWord($num)
+                    {
+                        switch ($num)
+                        {
+                            case "1":
+                                return "one";
+                            case "2":
+                                return "two";
+                            case "3":
+                                return "three";
+                            case "4":
+                                return "four";
+                            case "5":
+                                return "five";
+                            case "6":
+                                return "six";
+                            break;
+                        }
+                }
+
                 $stepStr = $row['all_steps'];
                 $stepImgStr = $row['step_imgs'];
 
@@ -102,6 +122,7 @@
                 // print_r($stepArray);
                 // echo "<br>";
                 // print_r($stepImgArray);
+
 
                 for($lp = 0; $lp < count($stepImgArray); $lp++){
                     $step = $stepArray[$lp];
@@ -114,17 +135,22 @@
                         $cnt += $lp;
                     }
 
+                    $stepHead = substr($stepArray[$cnt], 2);
+                    $stepNum = $stepArray[$cnt][0];
                     // echo $cnt;
 
+                    $stepNumWord = stepToWord($stepNum);
+                    // echo $stepNumWord;
+
                     // find a way to add ID for the steps
-                    // also selecting the first character of the string for the st
                     echo "
-                    <div class=\"step\">
+                    <div class=\"step\" id=\"" . $stepNumWord . "\">
                         <div class=\"step_head\"> 
-                            <h3>" . $stepArray[$cnt] . "</h3>
+                            <h3>" . $stepNum . "</h3>
+                            <h4>" . $stepHead . "</h4>
                             <img src=\"img/" . $stepImg . "\" alt=\"Step Img\">
                         </div>
-                        <p>". $stepArray[$cnt + 1] . "</p> 
+                        <p>" . $stepArray[$cnt + 1] . "</p> 
                     </div>";
                    
                     
@@ -133,7 +159,7 @@
                 ?>
                 
                 <!-- <div id="old">
-                    <div class="step" class="odd">
+                    <div class="step" id="one">
                         <div class="step_head">
                             <h3>1</h3>
                             <h4>Prepare the ingredients</h4>
@@ -141,7 +167,7 @@
                         </div>
                         <p>Remove the <b>dough</b> from the refrigerator to bring to room temperature. Place an oven rack in the center of the oven, then preheat to 475°F. Wash and dry the fresh produce. Cut off and discard the bottom 1/2 inch of the broccoli stem, then roughly chop the <b>broccoli</b>. Peel and roughly chop the <b>garlic</b>. Tear the <b>mozzarella cheese</b> into small pieces. Quarter and deseed the <b>lemon</b>.</p>
                     </div>
-                    <div class="step" class="even">
+                    <div class="step" id="two">
                         <div class="step_head">
                             <h3>2</h3>
                             <h4>Cook the broccoli & make the filling</h4>
@@ -150,7 +176,7 @@
                         <p>In a large pan (nonstick, if you have one), heat 2 teaspoons of olive oil on medium-high until hot. Add the <b>chopped broccoli</b>; season with salt and pepper. Cook, stirring occasionally, 4 to 6 minutes, or until lightly browned. Add <b>2/3 of the chopped garlic</b>. Cook, stirring constantly, 30 seconds to 1 minute, or until fragrant. Add <b>1/4 cup of water</b>; season with salt and pepper. Cook, stirring occasionally, 2 to 3 minutes, or until the broccoli has softened and the water has cooked off. Transfer to a large bowl. Add the mozzarella cheese, ricotta cheese, half the Italian seasoning, and the juice of 1 lemon wedge; stir to combine. Season with salt and pepper.
                             Wipe out the pan.</p>
                     </div>
-                    <div class="step" class="odd">
+                    <div class="step" id="three">
                         <div class="step_head">
                             <h3>3</h3>
                             <h4>Assemble & bake the calzones</h4>
@@ -158,7 +184,7 @@
                         </div>
                         <p>Lightly oil a sheet pan. Divide the <b>dough</b> into 2 equal-sized portions; using your hands and a rolling pin (or wine bottle), gently stretch and roll the portions into ¼-inch-thick rounds. (If the dough is resistant, let rest for 5 minutes.) Divide the <b>filling</b> between the centers of the rounds; fold each round in half over the filling. Using a fork, crimp the edges of the dough to seal. Transfer to the sheet pan. Using a fork, poke a few holes across the tops of the calzones to vent. Lightly drizzle the calzones with olive oil. Bake 16 to 18 minutes, or until golden brown. Transfer to a cutting board and let stand for at least 2 minutes.</p>
                     </div>
-                    <div class="step" class="even">
+                    <div class="step" id="four">
                         <div class="step_head">
                             <h3>4</h3>
                             <h4>Prepare the remaining ingredients</h4>
@@ -166,7 +192,7 @@
                         </div>
                         <p>While the calzones bake, using the flat side of your knife, smash the <b>olives</b>; remove and discard the pits, then roughly chop. Cut off and discard the root end of the <b>lettuce</b>; roughly chop the leaves. To make the dressing, in a large bowl, combine the <b>mayonnaise, half the parmesan cheese, the juice of the remaining lemon wedges</b>, and a drizzle of olive oil. Season with salt and pepper to taste.</p>
                     </div>
-                    <div class="step" class="odd">
+                    <div class="step" id="five">
                         <div class="step_head">
                             <h3>5</h3>
                             <h4></h4>
@@ -174,7 +200,7 @@
                         </div>
                         <p>While the calzones continue to bake, in the pan used to cook the broccoli,heat 2 teaspoons of olive oil on medium-high until hot. Add the <b>remaining chopped garlic</b>; cook, stirring constantly, 30 seconds to 1 minute, or until fragrant. Add the <b>tomato sauce</b> and remaining <b>Italian seasoning</b>. Cook, stirring frequently, 2 to 3 minutes, or until slightly thickened. Turn off the heat and season with salt and pepper to taste.</p>
                     </div>
-                    <div class="step" class="even">
+                    <div class="step" id="six">
                         <div class="step_head">
                             <h3>6</h3>
                             <h4>Make the salad & serve your dish</h4>
@@ -182,8 +208,8 @@
                         </div>
                         <p>Just before serving, add the <b>chopped olives and lettuce</b> to the bowl of dressing. Toss to coat; season with salt and pepper to taste. Cut the <b>baked calzones</b> in half. Serve with the sauce and salad on the side. Garnish with the <b>remaining parmesan cheese</b>. Enjoy!</p>
                     </div>
-                </div> -->
-            </div>
+                </div>
+            </div> -->
         </main>
         <!-- <footer>
             <a href="index.php">Home</a>
